4262499d08b1b0006159f0666b2b531e085f46a2
Umar Solanki <uysolanki@gmail.com>
2025-03-27 12:26:24 +0530

    rd

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String username=request.getParameter("t1");
+		String password=request.getParameter("t2");
+		
+		if(username.endsWith("itp.com") && password.length()>=6)
+		{
+			RequestDispatcher rd=request.getRequestDispatcher("/homepage.html");
+			rd.forward(request, response);
+		}
+		else
+		{
+			out.print("Invalid Credentials");
+			RequestDispatcher rd=request.getRequestDispatcher("/login.html");
+			rd.include(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
