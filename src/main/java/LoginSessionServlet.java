24686a210bc88c2c07de4f6b311fd48d032b4e7e
Umar Solanki <uysolanki@gmail.com>
2025-04-01 12:29:27 +0530

    sesson

---
+++ src/main/java/LoginSessionServlet.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginSessionServlet
+ */
+@WebServlet("/LoginSessionServlet")
+public class LoginSessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginSessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String username=request.getParameter("t1");
+		String password=request.getParameter("t2");
+		
+		out.print("Welcome ," + username);
+		HttpSession session=request.getSession(true);
+		session.setAttribute("loggedinuser", username);
+		
+		out.print("<a href='shop'>GO TO SHOPPING</a>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
